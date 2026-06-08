cb14792eee8549bfbd0d139b077070ea1bb5cb1c
bamidele <morenikejisunday12@gmail.com>
2026-06-08 10:06:44 +0100

    fix

--- src/mailer.ts
+++ src/mailer.ts
@@ -1,14 +1,14 @@
 import nodemailer from "nodemailer";
 
-const {
-  SMTP_HOST,
-  SMTP_PORT,
-  SMTP_SECURE,
-  SMTP_USER,
-  SMTP_PASS,
-} = process.env;
+const { SMTP_HOST, SMTP_PORT, SMTP_SECURE, SMTP_USER, SMTP_PASS } = process.env;
 
 if (!SMTP_HOST || !SMTP_PORT || !SMTP_USER || !SMTP_PASS) {
+  console.error({
+    SMTP_HOST: !!SMTP_HOST,
+    SMTP_PORT: !!SMTP_PORT,
+    SMTP_USER: !!SMTP_USER,
+    SMTP_PASS: !!SMTP_PASS,
+  });
   throw new Error("Missing required SMTP_* environment variables");
 }
 
